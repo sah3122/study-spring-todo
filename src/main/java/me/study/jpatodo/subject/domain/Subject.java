0e474e7d9cf39de40cc82cbe0bf4308bce689ae4
dongchul <sah3122@naver.com>
2020-04-06 22:40:28 +0900

    feat(Subject): add Subject Entity

--- src/main/java/me/study/jpatodo/subject/domain/Subject.java
+++ src/main/java/me/study/jpatodo/subject/domain/Subject.java
@@ -1,4 +1,21 @@
 package me.study.jpatodo.subject.domain;
 
-public class Subject {
+import me.study.jpatodo.card.domain.Card;
+import me.study.jpatodo.common.entity.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Subject extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    @OneToMany(mappedBy = "subject")
+    private List<Card> cards = new ArrayList<>();
 }
