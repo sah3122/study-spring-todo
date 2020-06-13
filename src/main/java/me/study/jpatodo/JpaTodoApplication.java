55d26d46962062de93eeb5f4b1867438eb4136cf
dongchul <sah3122@naver.com>
2020-06-13 23:08:29 +0900

    feat(Account): add test account

--- src/main/java/me/study/jpatodo/JpaTodoApplication.java
+++ src/main/java/me/study/jpatodo/JpaTodoApplication.java
@@ -1,9 +1,16 @@
 package me.study.jpatodo;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.study.jpatodo.auth.application.AccountService;
+import me.study.jpatodo.auth.domain.AccountRepository;
+import me.study.jpatodo.auth.dto.AccountRequest;
+import me.study.jpatodo.auth.dto.AccountRequest.AccountCreateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
@@ -12,6 +19,9 @@ import javax.persistence.EntityManager;
 @SpringBootApplication
 public class JpaTodoApplication {
 
+    @Autowired
+    private AccountService accountService;
+
     public static void main(String[] args) {
         SpringApplication.run(JpaTodoApplication.class, args);
     }
@@ -19,5 +29,14 @@ public class JpaTodoApplication {
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
         return new JPAQueryFactory(entityManager);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        AccountCreateRequest accountCreateRequest = new AccountCreateRequest();
+        accountCreateRequest.setNickname("dong1");
+        accountCreateRequest.setEmail("dong1@gmail.com");
+        accountCreateRequest.setPassword("password");
+        accountService.saveAccount(accountCreateRequest);
     }
 }
